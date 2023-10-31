d19ad6e1d7301ca094585391021258a6856fe97b
Oleksandr Vershyhora <dvershihora@gmail.com>
2023-10-31 23:05:14 +0100

    API Architecture

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,5 +1,7 @@
+using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 
 namespace Infrastructue.Data
 {
@@ -10,5 +12,15 @@ namespace Infrastructue.Data
         }
 
         public DbSet<Product> Products { get; set; }
+        
+        public DbSet<ProductBrand> ProductBrands {get; set;}
+
+        public DbSet<ProductType> ProductTypes {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
